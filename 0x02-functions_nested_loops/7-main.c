0596e145d19c656162adf02882f61225535aed5c
Sonkei07 <moueqqitamine1996@gmail.com>
2023-06-21 06:58:04 -0700

    fix doc task7

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -1,13 +1,12 @@
-#include "main.h"	
+#include "main.h"
 /**
 * main - check the code
 *
 * Return: Always 0.
 */
-int main(void)		
-{	
+int main(void)
+{
     int r;
-
     print_last_digit(98);
     print_last_digit(0);
     r = print_last_digit(-1024);
